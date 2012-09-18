a8f8b42a9337c443851f79fa41b1f1d5baca67d5
jason peak <jpeak5@lsu.edu>
2012-09-17 20:38:11 -0500

    adding nice getopts functionality

--- zf2.sh
+++ zf2.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
+
+# thanks to the author of the following for some nice intro to getopts
+# http://wiki.bash-hackers.org/howto/getopts_tutorial
+
 usage="usage: $0 <Module Name>; use --help for more info";
 if [ $# != 1 ]
 then
 	echo $usage 
-elif [ $# == 1 ]
-then
-		lower="$(echo ${1} | tr 'A-Z' 'a-z')"
-		mkdir -p "$1/config" "$1/src/$1/Controller" "$1/src/$1/Form" "$1/src/$1/Model" "$1/view/$lower/$lower"
-		tree $1;
 fi
+
+while getopts ":m:" opt; do
+  case $opt in
+    m)
+      echo "-m creating module $OPTARG..." >&2
+	lower="$(echo ${OPTARG} | tr 'A-Z' 'a-z')"
+	mkdir -p "$OPTARG/config" "$OPTARG/src/$OPTARG/Controller" "$OPTARG/src/$OPTARG/Form" "$OPTARG/src/$OPTARG/Model" "$OPTARG/view/$lower/$lower"
+	tree $OPTARG;
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+
 exit 0;
