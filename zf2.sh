84963257e5a6cdcee0d492c268b14f6648f7de5b
jason peak <jpeak5@lsu.edu>
2012-09-17 20:50:34 -0500

    few little bugs...straight now, ultra small and simple

--- zf2.sh
+++ zf2.sh
@@ -3,19 +3,22 @@
 # thanks to the author of the following for some nice intro to getopts
 # http://wiki.bash-hackers.org/howto/getopts_tutorial
 
-usage="usage: $0 <Module Name>; use --help for more info";
+usage="usage: $0 -m <ModuleName>; use -h for more info";
 if [ $# != 1 ]
 then
 	echo $usage 
 fi
 
-while getopts ":m:" opt; do
+while getopts ":m:h" opt; do
   case $opt in
     m)
       echo "-m creating module $OPTARG..." >&2
 	lower="$(echo ${OPTARG} | tr 'A-Z' 'a-z')"
 	mkdir -p "$OPTARG/config" "$OPTARG/src/$OPTARG/Controller" "$OPTARG/src/$OPTARG/Form" "$OPTARG/src/$OPTARG/Model" "$OPTARG/view/$lower/$lower"
 	tree $OPTARG;
+      ;;
+    h)
+      echo "there's not much to it. Give the name of your new module as argument to the -m (for module) flag..."
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
